5f273483ee56a56733d7e0858c4951e7ba39164b
AnasMuhammad-Sait <166283439+AnasMuhammad-Sait@users.noreply.github.com>
2025-07-25 11:53:03 +0530

    Create QuickExercise.py

---
+++ 25th_July/QuickExercise.py
@@ -0,0 +1,10 @@
+movie = "The Lion king"
+print(movie[4:9])
+
+arr =["Fried Chicken", "Watermelon", "KoolAid","Mashed Potatoes"]
+arr.append("Fish and Chips")
+arr.remove("Fish and Chips")
+print(arr)
+
+tup = (1,2,3)
+print(tup[2])
